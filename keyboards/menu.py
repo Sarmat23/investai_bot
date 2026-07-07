11733a63d4013dabea4dc483668af5a3ce185158
Sarmat23 <79203632217@yandex.ru>
2026-07-07 09:30:29 +0300

    Update menu.py

--- keyboards/menu.py
+++ keyboards/menu.py
@@ -1,1 +1,11 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+def main_menu():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="📊 Портфель")],
+            [KeyboardButton(text="➕ Добавить актив")],
+            [KeyboardButton(text="📈 Анализ")],
+        ],
+        resize_keyboard=True
+    )
